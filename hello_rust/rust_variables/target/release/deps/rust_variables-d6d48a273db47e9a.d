d220d08082da3bda2f4235a161e48ff45316c0b2
01Qin <sisukasqin@hotmail.com>
2025-05-28 13:14:21 +0300

    fix the bug

---
+++ hello_rust/rust_variables/target/release/deps/rust_variables-d6d48a273db47e9a.d
@@ -0,0 +1,5 @@
+/Users/snowwhite/rust/hello_rust/rust_variables/target/release/deps/rust_variables-d6d48a273db47e9a: src/main.rs
+
+/Users/snowwhite/rust/hello_rust/rust_variables/target/release/deps/rust_variables-d6d48a273db47e9a.d: src/main.rs
+
+src/main.rs:
---
+++ hello_rust/rust_variables/target/release/rust_variables.d
@@ -0,0 +1,1 @@
+/Users/snowwhite/rust/hello_rust/rust_variables/target/release/rust_variables: /Users/snowwhite/rust/hello_rust/rust_variables/src/main.rs
