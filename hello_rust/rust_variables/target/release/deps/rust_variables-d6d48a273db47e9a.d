dde3aa0b5c5a1855a0259c1fea3b3637b6fd1c9d
Quinn <144977657+01Qin@users.noreply.github.com>
2025-05-29 12:01:23 +0300

    Delete hello_rust/rust_variables/target directory

--- hello_rust/rust_variables/target/release/deps/rust_variables-d6d48a273db47e9a.d
+++
@@ -1,5 +0,0 @@
-/Users/snowwhite/rust/hello_rust/rust_variables/target/release/deps/rust_variables-d6d48a273db47e9a: src/main.rs
-
-/Users/snowwhite/rust/hello_rust/rust_variables/target/release/deps/rust_variables-d6d48a273db47e9a.d: src/main.rs
-
-src/main.rs:
