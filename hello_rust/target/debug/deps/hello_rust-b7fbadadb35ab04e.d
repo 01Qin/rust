769026185eae78262f5b5f84390e7b703a162ae5
Quinn <144977657+01Qin@users.noreply.github.com>
2025-05-29 12:00:56 +0300

    Delete hello_rust/target directory

--- hello_rust/target/debug/deps/hello_rust-b7fbadadb35ab04e.d
+++
@@ -1,5 +0,0 @@
-/Users/snowwhite/rust/hello_rust/target/debug/deps/libhello_rust-b7fbadadb35ab04e.rmeta: src/main.rs
-
-/Users/snowwhite/rust/hello_rust/target/debug/deps/hello_rust-b7fbadadb35ab04e.d: src/main.rs
-
-src/main.rs:
